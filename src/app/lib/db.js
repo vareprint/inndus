ec0979f2e73f507540d05cf3a3788c79fd27bf04
Your Name <you@example.com>
2025-07-08 18:00:04 +0530

    update config

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -7,9 +7,9 @@ export async function getConnection() {
   if (!pool) {
     pool = mysql.createPool({
       host: "localhost",
-      user: "root",
-      password: "",
-      database: "inndus_new",
+      user: "vareprint_internal",
+      password: "Vareprint@25",
+      database: "vareprint_inndus_interanal",
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
